123c04af676abc92710d43a252f6f1f2852db4b9
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-26 17:26:24 +0900

    feat: 유저 삭제 API 구현

--- src/main/java/app/fitsync/domain/user/controller/UserController.java
+++ src/main/java/app/fitsync/domain/user/controller/UserController.java
@@ -1,10 +1,12 @@
 package app.fitsync.domain.user.controller;
 
+import app.fitsync.domain.user.dto.UserDeleteRequest;
 import app.fitsync.domain.user.dto.UserRequest;
 import app.fitsync.domain.user.dto.UserResponse;
 import app.fitsync.domain.user.service.UserServiceInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,12 @@ public class UserController {
     @PostMapping("/api/user")
     public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request) {
         UserResponse response = userService.createUser(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/api/user")
+    public ResponseEntity<UserResponse> deleteUser(@RequestBody UserDeleteRequest request) {
+        UserResponse response = userService.deleteUser(request);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/app/fitsync/domain/user/service/UserServiceInterface.java
+++ src/main/java/app/fitsync/domain/user/service/UserServiceInterface.java
@@ -1,8 +1,11 @@
 package app.fitsync.domain.user.service;
 
+import app.fitsync.domain.user.dto.UserDeleteRequest;
 import app.fitsync.domain.user.dto.UserRequest;
 import app.fitsync.domain.user.dto.UserResponse;
 
 public interface UserServiceInterface {
     UserResponse createUser(UserRequest request);
+
+    UserResponse deleteUser(UserDeleteRequest request);
 }
