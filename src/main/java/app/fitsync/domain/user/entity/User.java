2bd515b8481a5952aa3cc121c20634aac70e6e26
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-26 16:03:40 +0900

    fix(user): 유저 엔티티 미사용 import 제거

--- src/main/java/app/fitsync/domain/user/entity/User.java
+++ src/main/java/app/fitsync/domain/user/entity/User.java
@@ -8,13 +8,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
