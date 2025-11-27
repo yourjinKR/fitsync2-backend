51f9f5e507a5e88c03ac28726818a335ac827d65
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-27 20:28:14 +0900

    feat: 유저의 나이와 성별을 추가

---
+++ src/main/java/app/fitsync/domain/user/entity/BirthDate.java
@@ -0,0 +1,21 @@
+package app.fitsync.domain.user.entity;
+
+import jakarta.persistence.Embeddable;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+public class BirthDate {
+    private LocalDateTime birth;
+
+    public BirthDate(LocalDateTime localDateTime) {
+        this.birth = localDateTime;
+    }
+
+    public Long getAge() {
+        LocalDateTime now = LocalDateTime.now();
+        return ChronoUnit.YEARS.between(birth, now);
+    }
+}
---
+++ src/main/java/app/fitsync/domain/user/entity/Gender.java
@@ -0,0 +1,6 @@
+package app.fitsync.domain.user.entity;
+
+public enum Gender {
+    MALE,
+    FEMALE
+}
