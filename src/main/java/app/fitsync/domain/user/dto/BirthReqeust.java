ad618f3c761190b7470fc0b97f6e8fed7eb7edf1
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-27 20:28:47 +0900

    refactor: 추가된 컬럼에 맞게 매핑 로직 수정 추가된 컬럼 : 성별, 나이

---
+++ src/main/java/app/fitsync/domain/user/dto/BirthReqeust.java
@@ -0,0 +1,8 @@
+package app.fitsync.domain.user.dto;
+
+public record BirthReqeust(
+        int year,
+        int month,
+        int dayOfMonth
+) {
+}
--- src/main/java/app/fitsync/domain/user/dto/UserRequest.java
+++ src/main/java/app/fitsync/domain/user/dto/UserRequest.java
@@ -1,9 +1,13 @@
 package app.fitsync.domain.user.dto;
 
+import app.fitsync.domain.user.entity.Gender;
+
 public record UserRequest(
         String loginId,
         String password,
-        String name
+        String name,
+        Gender gender,
+        BirthReqeust birth
 ) {
 
 }
