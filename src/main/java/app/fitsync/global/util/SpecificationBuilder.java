29ca249c01d8d64183857c836bd9562437fc4dac
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-12-15 17:47:00 +0900

    refactor : Path와 Root를 활용하여 체이닝하여 접근

--- src/main/java/app/fitsync/global/util/SpecificationBuilder.java
+++ src/main/java/app/fitsync/global/util/SpecificationBuilder.java
@@ -1,5 +1,7 @@
 package app.fitsync.global.util;
 
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.jpa.domain.Specification;
@@ -12,16 +14,36 @@ public class SpecificationBuilder<T> {
         return new SpecificationBuilder<>();
     }
 
+    // 경로 탐색 헬퍼 메서드 추가
+    private Path<?> getPath(Root<T> root, String attributeName) {
+        if (attributeName.contains(".")) {
+            String[] paths = attributeName.split("\\.");
+            Path<?> path = root.get(paths[0]);
+            for (int i = 1; i < paths.length; i++) {
+                path = path.get(paths[i]);
+            }
+            return path;
+        }
+        return root.get(attributeName);
+    }
+
     public SpecificationBuilder<T> andEqual(String fieldName, Object value) {
         if (value != null) {
-            specs.add(((root, query, builder) -> builder.equal(root.get(fieldName), value)));
+            specs.add(((root, query, builder) ->
+                    builder
+                            .equal(getPath(root, fieldName), value))
+            );
         }
         return this;
     }
 
     public SpecificationBuilder<T> andLike(String fieldName, String value) {
         if (value != null && !value.trim().isEmpty()) {
-            specs.add((root, query, builder) -> builder.like(root.get(fieldName), "%" + value + "%"));
+            specs.add((root, query, builder) ->
+                    builder
+                            .like(getPath(root, fieldName)
+                            .as(String.class),"%" + value + "%")
+            );
         }
         return this;
     }
