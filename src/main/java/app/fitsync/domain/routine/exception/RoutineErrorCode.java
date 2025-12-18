a9ed0a2458ddbd3a9b78df3829507126d9083b5e
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-12-18 19:52:19 +0900

    feat: 운동 루틴에 대한 에러 코드 관리

---
+++ src/main/java/app/fitsync/domain/routine/exception/RoutineErrorCode.java
@@ -0,0 +1,23 @@
+package app.fitsync.domain.routine.exception;
+
+import app.fitsync.global.exception.ErrorCode;
+import java.text.MessageFormat;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum RoutineErrorCode implements ErrorCode {
+    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID와 일치하는 운동 루틴을 찾지 못했습니다 ID : {0}"),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+
+    @Override
+    public String format(Object... args) {
+        return MessageFormat.format(message, args);
+    }
+}
