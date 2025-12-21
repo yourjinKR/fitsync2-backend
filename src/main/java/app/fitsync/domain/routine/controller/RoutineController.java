cac41e3613991323f6961e0bb11351e11f95bfc5
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-12-21 10:47:57 +0900

    refactor: RestController 어노테이션 수정

--- src/main/java/app/fitsync/domain/routine/controller/RoutineController.java
+++ src/main/java/app/fitsync/domain/routine/controller/RoutineController.java
@@ -1,7 +1,5 @@
 package app.fitsync.domain.routine.controller;
 
-import app.fitsync.domain.exercise.dto.exercise.ExerciseListResponse;
-import app.fitsync.domain.exercise.entity.ExerciseCategory;
 import app.fitsync.domain.routine.dto.routine.RoutineDetailResponse;
 import app.fitsync.domain.routine.dto.routine.RoutineListResponse;
 import app.fitsync.domain.routine.dto.routine.RoutineRequest;
@@ -14,15 +12,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequiredArgsConstructor
-@Controller
 public class RoutineController {
 
     private final RoutineServiceInterface routineService;
