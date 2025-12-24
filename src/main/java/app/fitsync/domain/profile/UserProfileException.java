1b78864c0fd9157e51e441bf0d9139a667d157f6
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-12-24 18:22:58 +0900

    feat: 유저 및 유저 프로필 커스텀 에러 코드 추가

---
+++ src/main/java/app/fitsync/domain/profile/UserProfileException.java
@@ -0,0 +1,24 @@
+package app.fitsync.domain.profile;
+
+import app.fitsync.global.exception.ErrorCode;
+import java.text.MessageFormat;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserProfileException implements ErrorCode {
+    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID와 일치하는 유저 프로필을 찾지 못했습니다 ID : {0}"),
+    DUPLICATE(HttpStatus.CONFLICT, "이미 해당 유저의 프로필이 존재합니다 ID : {0}")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+
+    @Override
+    public String format(Object... args) {
+        return MessageFormat.format(message, args);
+    }
+}
