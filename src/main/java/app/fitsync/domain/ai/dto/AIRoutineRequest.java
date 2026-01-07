152a0b62ac27f566b02b2054f7553ed091c38388
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-01-07 18:00:45 +0900

    refactor: AI 운동 루틴 요청 DTO 수정

--- src/main/java/app/fitsync/domain/ai/dto/AIRoutineRequest.java
+++ src/main/java/app/fitsync/domain/ai/dto/AIRoutineRequest.java
@@ -4,10 +4,12 @@ import app.fitsync.domain.profile.dto.UserProfileDetailResponse;
 import app.fitsync.domain.user.dto.UserHeaderInfoResponse;
 
 /**
- * AI 운동 루틴 추천 서비스 요청<br>
+ * <h3>AI 운동 루틴 추천 서비스 요청</h1><br>
+ * - 요청 루틴 분할 수<br>
  * - 사용자 정보
  */
 public record AIRoutineRequest(
+        Integer splitCount,
         UserInfo userInfo
 ) {
     public record UserInfo(
