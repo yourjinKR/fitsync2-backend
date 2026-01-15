f86865fe0260e32711089adb2f9b59226fe52396
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-01-15 22:24:17 +0900

    todo: 검색 조건 todo 추가

--- src/main/java/app/fitsync/domain/workout/dto/WorkoutListRequest.java
+++ src/main/java/app/fitsync/domain/workout/dto/WorkoutListRequest.java
@@ -2,6 +2,7 @@ package app.fitsync.domain.workout.dto;
 
 public record WorkoutListRequest(
         long ownerId
+        // TODO : 검색 조건 추가
         // writerId
         // date ~ date
         // category
