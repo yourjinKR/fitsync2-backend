f202b546d833e323e3834e8eb113fcc58b2e1a40
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-01-29 19:57:09 +0900

    chore: 환경변수 체크 및 scope값 변경

--- src/main/java/app/fitsync/global/EnvDebug.java
+++ src/main/java/app/fitsync/global/EnvDebug.java
@@ -18,6 +18,8 @@ public class EnvDebug implements CommandLineRunner {
         System.out.println("OPEN_AI_SECRET_KEY = " + env.getProperty("OPEN_AI_SECRET_KEY"));
         System.out.println(env.getProperty("NAVER_CLIENT_ID"));
         System.out.println(env.getProperty("NAVER_SECRET_KEY"));
+        System.out.println(env.getProperty("GOOGLE_CLIENT_ID"));
+        System.out.println(env.getProperty("GOOGLE_SECRET_KEY"));
 
         System.out.println("spring.ai.openai.api-key = " +
                 env.getProperty("spring.ai.openai.api-key"));
