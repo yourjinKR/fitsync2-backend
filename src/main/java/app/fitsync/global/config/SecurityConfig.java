990f08d2558b7da9be84f0e24e90cb193e64beb1
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-02-03 19:14:32 +0900

    fix: 권한 검사를 hasAnyAuthority로 하도록 수정

--- src/main/java/app/fitsync/global/config/SecurityConfig.java
+++ src/main/java/app/fitsync/global/config/SecurityConfig.java
@@ -122,10 +122,10 @@ public class SecurityConfig {
 
         // 인가
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/jwt/exchange", "/jwt/refresh").permitAll()
+                .requestMatchers("/jwt/exchange", "/jwt/refresh", "/error").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/user/exist", "/api/user").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/check").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/user/me").hasAnyRole("MEMBER", "TRAINER", "ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/user/me").hasAnyAuthority("MEMBER", "TRAINER", "ADMIN")
                 .anyRequest().authenticated()
         );
 
