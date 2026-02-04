fa02dee1b093b6abc38eee6a30278e682f52d350
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-02-04 17:22:39 +0900

    Merge pull request #42
    * feat: SecurityContext 접근 Facade
    
    * feat: 유저 정보를 불러오는 Provider 유틸 구현
    
    * refactor: 자신의 정보 조회 로직 수정

---
+++ src/main/java/app/fitsync/domain/user/CurrentUserProvider.java
@@ -0,0 +1,33 @@
+package app.fitsync.domain.user;
+
+import app.fitsync.domain.user.entity.User;
+import app.fitsync.domain.user.exception.UserException;
+import app.fitsync.domain.user.repository.UserRepository;
+import app.fitsync.global.exception.RestApiException;
+import app.fitsync.global.security.SecurityFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class CurrentUserProvider {
+
+    private final SecurityFacade securityFacade;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User getUser() {
+
+        String loginId = securityFacade.getLoginIdOrThrow();
+
+        return userRepository.findByLoginIdAndHiddenIsFalse(loginId)
+                .orElseThrow(() -> new RestApiException(UserException.NOT_FOUND_LOGIN_ID, loginId));
+
+    }
+
+    @Transactional
+    public Long getUserId() {
+        return getUser().getId();
+    }
+}
