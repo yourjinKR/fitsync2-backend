f95f1a79d45339c3fd673ed6623d4a552dafd359
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-02-23 20:09:09 +0900

    feat: 신체부위 리스트 메서드 추가

--- src/main/java/app/fitsync/domain/exercise/repository/BodyDetailPartRepository.java
+++ src/main/java/app/fitsync/domain/exercise/repository/BodyDetailPartRepository.java
@@ -1,9 +1,13 @@
 package app.fitsync.domain.exercise.repository;
 
 import app.fitsync.domain.exercise.entity.BodyDetailPart;
+import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BodyDetailPartRepository extends JpaRepository<BodyDetailPart, Long> {
+    @EntityGraph(attributePaths = "bodyPart")
+    List<BodyDetailPart> findAllByOrderByIdAsc();
 }
